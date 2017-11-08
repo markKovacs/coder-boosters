9577a5514fc08e827077b0aef7ccbedf2daf628f
Mark Kovacs <kovacsmark89@gmail.com>
2017-11-08 18:03:19 +0100

    Data initialization enhanced

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -3,8 +3,18 @@ package application;
 import application.controller.AccountController;
 import application.controller.IndexController;
 import application.model.*;
+import application.model.account.BoosterAccount;
+import application.model.account.CustomerAccount;
+import application.model.account.GameAccount;
+import application.model.order.BoostOrder;
+import application.model.order.LeagueDivision;
+import application.model.order.LoLBoostOrder;
+import application.model.order.OrderType;
 import application.utils.Path;
 import javax.persistence.*;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
@@ -54,16 +64,34 @@ public class App {
         CustomerAccount customerAttila = new CustomerAccount("attila", "attila@attila.hu", "sha1:64000:18:oyEUTFE4hzrGFlAC+iX0mviGuaSk7/F3:SkY3/+/Yv7oDmga2KGBeNdcX");
         customerAttila.addGameAccountBiDir(new GameAccount("lol_king", "pass", GameType.LOL));
 
+        BoostOrder order1 = new LoLBoostOrder(LeagueDivision.BRONZE_III, 5, OrderType.GAMES_PLAYED,
+                10, getDate(2017, 12, 18, 22, 0));
+        customerMark.addBoostOrderBiDir(order1);
+
+        BoostOrder order2 = new LoLBoostOrder(LeagueDivision.SILVER_I, 10, OrderType.GAMES_WON,
+                10, getDate(2018, 6, 5, 10, 0));
+        customerMark.addBoostOrderBiDir(order2);
+
+        BoosterAccount booster1 = new BoosterAccount("barna", "barna@elite.com", "sha1:64000:18:oyEUTFE4hzrGFlAC+iX0mviGuaSk7/F3:SkY3/+/Yv7oDmga2KGBeNdcX");
+        booster1.addBoostOrderBiDir(order2);
+
         // BEGIN TRANSACTION
         em.getTransaction().begin();
 
         // PERSIST ENTITIES
         em.persist(customerMark);
         em.persist(customerAttila);
+        em.persist(booster1);
 
         // END TRANSACTION AND CLOSE ENTITY MANAGER
         em.getTransaction().commit();
         em.close();
+    }
+
+    private static Date getDate(int y, int m, int d, int h, int min) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(y, m, d, h, min);
+        return cal.getTime();
     }
 
 }
