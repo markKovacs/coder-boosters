54e71a56c34b46904aa9bd8d05e7c960faa998da
BenceZambo <bence.zambo96@gmail.com>
2017-11-09 15:00:00 +0100

    choose game page route and render created

--- src/main/java/application/utils/Path.java
+++ src/main/java/application/utils/Path.java
@@ -11,6 +11,7 @@ public class Path {
         public static final String CUSTOMER_PROFILE_EDIT_SUCCESS = "/customer-profile?edited=successful";
         public static final String BOOSTER_PROFILE_EDIT_SUCCESS = "/booster-profile?edited=successful";
         public static final String LOGOUT = "/logout";
+        public static final String CHOOSE_GAME = "/choose-game";
     }
 
     public static class Template {
@@ -19,6 +20,7 @@ public class Path {
         public static final String LOGIN = "login";
         public static final String CUSTOMER_PROFILE = "customer_profile";
         public static final String BOOSTER_PROFILE = "booster_profile";
+        public static final String CHOOSE_GAME = "choose_game";
     }
 
 }
