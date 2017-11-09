8f5f684cb8c4b0ee165b083de16bcf2e9c616aa2
hankoattila <hattilla4@gmail.com>
2017-11-09 15:41:33 +0100

    Html templates replaced to packages

--- src/main/java/application/controller/OrderController.java
+++ src/main/java/application/controller/OrderController.java
@@ -1,4 +1,17 @@
 package application.controller;
 
+import application.utils.Path;
+import application.utils.ViewUtil;
+import spark.Route;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class OrderController {
+    public static Route serveOrdersPage = (request, response) -> {
+
+        Map<String, Object> model = new HashMap<>();
+
+        return ViewUtil.render(request, model, Path.Template.ORDERS_PAGE);
+    };
 }
--- src/main/java/application/utils/Path.java
+++ src/main/java/application/utils/Path.java
@@ -11,14 +11,19 @@ public class Path {
         public static final String CUSTOMER_PROFILE_EDIT_SUCCESS = "/customer-profile?edited=successful";
         public static final String BOOSTER_PROFILE_EDIT_SUCCESS = "/booster-profile?edited=successful";
         public static final String LOGOUT = "/logout";
+        public static final String ORDERS_PAGE = "/orders-page";
+
+
     }
 
     public static class Template {
-        public final static String INDEX = "index";
-        public static final String REGISTER = "register";
-        public static final String LOGIN = "login";
-        public static final String CUSTOMER_PROFILE = "customer_profile";
-        public static final String BOOSTER_PROFILE = "booster_profile";
+        public final static String INDEX = "index/index";
+        public static final String REGISTER = "index/register";
+        public static final String LOGIN = "index/login";
+        public static final String CUSTOMER_PROFILE = "account/customer_profile";
+        public static final String BOOSTER_PROFILE = "account/booster_profile";
+        public static final String ORDERS_PAGE = "account/orders-page";
+
     }
 
 }
