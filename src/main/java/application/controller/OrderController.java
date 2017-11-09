298e138a4d617d746cb1a99673035ff32a8f1d87
BenceZambo <bence.zambo96@gmail.com>
2017-11-09 18:13:51 +0100

    order form created without css and js

--- src/main/java/application/controller/OrderController.java
+++ src/main/java/application/controller/OrderController.java
@@ -1,11 +1,12 @@
 package application.controller;
 
 import application.model.GameType;
+import application.model.order.LeagueDivision;
 import application.utils.Path;
 import application.utils.ViewUtil;
 import spark.Route;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
 
 public class OrderController {
     public static Route serveChooseGamePage = (request, response) -> {
@@ -18,9 +19,12 @@ public class OrderController {
     public static Route serveOrderForm = (request, response) -> {
 
         String gameTypeString = request.queryParams("game_type");
+        List<LeagueDivision> leagueDivisions = Arrays.asList(LeagueDivision.values());
+
 
         Map<String, Object> model = new HashMap<>();
         model.put("game_type", gameTypeString);
+        model.put("league_divisions", leagueDivisions);
 
         return ViewUtil.render(request, model, Path.Template.ORDER_FORM);
     };
