ab98cbf5400a82f92d76e23bcd0916bfc8385a5d
BenceZambo <bence.zambo96@gmail.com>
2017-12-07 14:59:29 +0100

    select game debugged, and css is a bit better than it was

--- src/main/resources/public/js/logic.js
+++ src/main/resources/public/js/logic.js
@@ -7,18 +7,14 @@ app.logic = {
     var panels = document.querySelectorAll('.panel');
     $(panels).each(app.logic.setClassToOrigin);
     this.classList.toggle('open');
-    },
-
-    toggleActive: function (e) {
-    if (e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');
-    }
+    this.classList.toggle('open-active');
     },
 
     setClassToOrigin: function (){
     var currentClassList = this.classList;
     if(this.className.includes('open')){
         currentClassList.toggle('open');
+        currentClassList.toggle('open-active');
     }
     },
 
